bf63430114ad0ed7857e00b7cdcc9345620ca4ce
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-02 22:37:51 -0300

    Chamando todos os dados que precisam para copiar a tela do usuário do Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({
+        name: '',
+        login: '',
+        avatar: '',
+    });
+
+    const [repos, setRepos] = useState([]);
+
+
+    const [search, setSearch] = useState('MatheusGomesRocha');
+
+    const getData = () => {
+        Promise.all([
+            fetch(`https://api.github.com/users/${search}`),
+            fetch(`https://api.github.com/users/${search}/repos`),
+        ]).then(async (response) => {
+            const userJson = await response[0].json();
+            const reposJson = await response[1].json();
+
+            setUser({login: userJson.login, name: userJson.name, avatar: userJson.avatar_url});
+            setRepos(reposJson);
+
+        })
+    }
+
+    useEffect(() => {
+        getData();
+    }, [setSearch])
+
+    const userSearch = (v) => {
+        setSearch(v.target.value);
+    }
+
+    return (
+        <div className="App">
+
+            <input onChange={userSearch} className={"input"}/>
+            <div className={"btn"} onClick={getData}>
+                olá mundo
+            </div>
+
+            <img width="100px" height={"100px"} src={user.avatar}/>
+
+            {repos.map((item, k) => (
+                <div key={k}>
+                    <span>{item.name}</span>
+                    <span>{item.language}</span>
+                </div>
+            ))}
+
+        </div>
+    );
 }
 
 export default App;
