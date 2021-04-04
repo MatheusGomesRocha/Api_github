7a4501e6c57b9868969114fb5cfdc29dcfbf29e0
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-04 11:15:54 -0300

    Adicionando botões para mudar o que ver, entre Overview e Repositories

--- src/App.js
+++ src/App.js
@@ -25,8 +25,11 @@ function App() {
 
     const [repos, setRepos] = useState([]);
     const [search, setSearch] = useState('MatheusGomesRocha');
+    const [qtdRepos, setQtdRepos] = useState();
 
     const [language, setLanguage] = useState('');
+
+    const [view, setView] = useState('Overview');
 
     const getData = () => {
         Promise.all([
@@ -49,6 +52,7 @@ function App() {
                 twitter: userJson.twitter_username,
             });
             setRepos(reposJson);
+            setQtdRepos(userJson.public_repos);
         })
     }
 
@@ -63,129 +67,147 @@ function App() {
 
     return (
         <div className="app">
-            <div className={"side"}>
-                <input onChange={userSearch} className={"input"}/>
-                <div className={"btn"} onClick={getData}>
-                    olá mundo
+
+            <div className={"changeView"}>
+                <div onClick={() => setView('Overview')} style={{width: 150, borderBottomColor: view == 'Overview' && '#F9826C'}} className={"defaultBtn"}>
+                    <EmailIcon />
+                    <span style={{color: view == 'Overview' && '#C3C6CA'}} className={"defaultText"}>Overview</span>
                 </div>
 
-                <img className={"img"} src={user.avatar}/>
-
-                <div className={"userInfo"}>
-                    <span className={"name"}>{user.name}</span>
-                    <span className={"login"}>{user.login}</span>
-                </div>
-
-                <div className={"btns"}>
-                    <div style={{alignItems: 'center'}} className={"btn"}>
-                        <span>Follow</span>
-                    </div>
-                    <div className={"btn"}>
-                        <span style={{fontSize: 18}}>...</span>
+                <div onClick={() => setView('Repositories')} style={{width: 220, borderBottomColor: view == 'Repositories' && '#F9826C'}} className={"defaultBtn"}>
+                    <EmailIcon />
+                    <span style={{color: view == 'Repositories' && '#C3C6CA'}} className={"defaultText"}>Repositories</span>
+                    <div className={"qtdRepos"}>
+                        <span style={{color: '#999', fontSize: 14}}>{qtdRepos}</span>
                     </div>
                 </div>
-
-                <span className={"bio"}>{user.bio}</span>
-
-                <div className={"followAndStars"}>
-                    <div className={"follow"}>
-                        <UsersIcon/>
-                        <span className={"followText"}><strong
-                            style={{color: '#C3C6CA', marginRight: 5}}>{user.followers}</strong>followers</span>
-                        <div className={"smallestBall"}></div>
-                        <span className={"followText"}><strong
-                            style={{color: '#C3C6CA', marginRight: 5}}>{user.following}</strong>following</span>
-                        <div className={"smallestBall"}></div>
-                    </div>
-
-                    <div className={"stars"}>
-                        <StarIcon/>
-                        <span className={"followText"}><strong style={{color: '#C3C6CA'}}>2.1k</strong></span>
-                    </div>
-                </div>
-
-                {user.email &&
-                <div className={"emailDiv"}>
-                    <EmailIcon/>
-                    <span className={"email"}>{user.email}</span>
-                </div>
-                }
-
-                <div className={"moreInfo"}>
-                    {user.location &&
-                    <div className={"defaultInfoDiv"}>
-                        <PinIcon/>
-                        <span className={"email"}>{user.location}</span>
-                    </div>
-                    }
-
-                    {user.blog &&
-                    <div className={"defaultInfoDiv"}>
-                        <LinkIcon/>
-                        <a href={user.blog} target={"_blank"} className={"link"}>{user.blog}</a>
-                    </div>
-                    }
-
-                    {user.twitter &&
-                    <div className={"defaultInfoDiv"}>
-                        <TwitterIcon/>
-                        <a href={"https://twitter.com/" + user.twitter} target={"_blank"}
-                           className={"link"}>@{user.twitter}</a>
-                    </div>
-                    }
-                </div>
-
-
             </div>
 
-            <div className={"side"}>
+            <div className={"mainDiv"}>
+                <div className={"side"}>
+                    <input onChange={userSearch} className={"input"}/>
+                    <div className={"btn"} onClick={getData}>
+                        olá mundo
+                    </div>
 
-                <div className={"arrayDiv"}>
-                    {repos.map((item, k) => (
-                        <div className={"arrayItem"} key={k}>
-                            <div className={"linkDiv"}>
-                                <a className={"link"} href={item.html_url}>{item.name}</a>
-                                <span className={"description"}>{item.description}</span>
-                            </div>
+                    <img className={"img"} src={user.avatar}/>
 
-                            <div className={"languageDiv"}>
-                                {item.language &&
-                                <>
-                                    <div style={{
-                                        backgroundColor:
-                                            item.language == 'JavaScript' && '#F1E05A' ||
-                                            item.language == 'Java' && '#B07219' ||
-                                            item.language == 'HTML' && '#E34C26' ||
-                                            item.language == 'Python' && '#3572A5' ||
-                                            item.language == 'GO' && '#00ADD8' ||
-                                            item.language == 'Shell' && '#89E051' ||
-                                            item.language == 'C' && '#555555' ||
-                                            item.language == 'PHP' && '#4F5D95' ||
-                                            item.language == 'PowerShell' && '#012456' ||
-                                            item.language == 'Pascal' && '#E3F171' ||
-                                            item.language == 'C++' && '#F34B7D' ||
-                                            item.language == 'Assembly' && '#6E4C13' ||
-                                            item.language == 'Ruby' && '#701516' ||
-                                            '#fff'
-                                    }} className={"ballFull"}/>
+                    <div className={"userInfo"}>
+                        <span className={"name"}>{user.name}</span>
+                        <span className={"login"}>{user.login}</span>
+                    </div>
 
-                                    <span className={"language"}>{item.language}</span>
-                                </>
-                                }
-                                <div className={"starDiv"}>
-                                    <StarIcon/>
-                                    <span className={"starText"}>{item.stargazers_count}</span>
-                                </div>
-
-                                <div className={"forkDiv"}>
-                                    <ForkIcon/>
-                                    <span className={"starText"}>{item.forks}</span>
-                                </div>
-                            </div>
+                    <div className={"btns"}>
+                        <div style={{alignItems: 'center'}} className={"btn"}>
+                            <span>Follow</span>
                         </div>
-                    ))}
+                        <div className={"btn"}>
+                            <span style={{fontSize: 18}}>...</span>
+                        </div>
+                    </div>
+
+                    <span className={"bio"}>{user.bio}</span>
+
+                    <div className={"followAndStars"}>
+                        <div className={"follow"}>
+                            <UsersIcon/>
+                            <span className={"followText"}><strong
+                                style={{color: '#C3C6CA', marginRight: 5}}>{user.followers}</strong>followers</span>
+                            <div className={"smallestBall"}></div>
+                            <span className={"followText"}><strong
+                                style={{color: '#C3C6CA', marginRight: 5}}>{user.following}</strong>following</span>
+                            <div className={"smallestBall"}></div>
+                        </div>
+
+                        <div className={"stars"}>
+                            <StarIcon/>
+                            <span className={"followText"}><strong style={{color: '#C3C6CA'}}>2.1k</strong></span>
+                        </div>
+                    </div>
+
+                    {user.email &&
+                    <div className={"emailDiv"}>
+                        <EmailIcon/>
+                        <span className={"email"}>{user.email}</span>
+                    </div>
+                    }
+
+                    <div className={"moreInfo"}>
+                        {user.location &&
+                        <div className={"defaultInfoDiv"}>
+                            <PinIcon/>
+                            <span className={"email"}>{user.location}</span>
+                        </div>
+                        }
+
+                        {user.blog &&
+                        <div className={"defaultInfoDiv"}>
+                            <LinkIcon/>
+                            <a href={user.blog} target={"_blank"} className={"link"}>{user.blog}</a>
+                        </div>
+                        }
+
+                        {user.twitter &&
+                        <div className={"defaultInfoDiv"}>
+                            <TwitterIcon/>
+                            <a href={"https://twitter.com/" + user.twitter} target={"_blank"}
+                               className={"link"}>@{user.twitter}</a>
+                        </div>
+                        }
+                    </div>
+
+
                 </div>
 
+                <div className={"side"}>
+
+                    <div className={"arrayDiv"}>
+                        {repos.map((item, k) => (
+                            <div className={"arrayItem"} key={k}>
+                                <div className={"linkDiv"}>
+                                    <a className={"link"} href={item.html_url}>{item.name}</a>
+                                    <span className={"description"}>{item.description}</span>
+                                </div>
+
+                                <div className={"languageDiv"}>
+                                    {item.language &&
+                                    <>
+                                        <div style={{
+                                            backgroundColor:
+                                                item.language == 'JavaScript' && '#F1E05A' ||
+                                                item.language == 'Java' && '#B07219' ||
+                                                item.language == 'HTML' && '#E34C26' ||
+                                                item.language == 'Python' && '#3572A5' ||
+                                                item.language == 'GO' && '#00ADD8' ||
+                                                item.language == 'Shell' && '#89E051' ||
+                                                item.language == 'C' && '#555555' ||
+                                                item.language == 'PHP' && '#4F5D95' ||
+                                                item.language == 'PowerShell' && '#012456' ||
+                                                item.language == 'Pascal' && '#E3F171' ||
+                                                item.language == 'C++' && '#F34B7D' ||
+                                                item.language == 'Assembly' && '#6E4C13' ||
+                                                item.language == 'Ruby' && '#701516' ||
+                                                '#fff'
+                                        }} className={"ballFull"}/>
+
+                                        <span className={"language"}>{item.language}</span>
+                                    </>
+                                    }
+                                    <div className={"starDiv"}>
+                                        <StarIcon/>
+                                        <span className={"starText"}>{item.stargazers_count}</span>
+                                    </div>
+
+                                    <div className={"forkDiv"}>
+                                        <ForkIcon/>
+                                        <span className={"starText"}>{item.forks}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
             </div>
         </div>
     );
