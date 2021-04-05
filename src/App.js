887c85dc7b848c72c6212fd641ab53c27db21ee9
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-05 09:47:18 -0300

    Adicionando novo array para mostrar repositories de outro jeito

--- src/App.js
+++ src/App.js
@@ -38,11 +38,14 @@ function App() {
     const handleScroll = () => {        // Verifica o Scroll da pagina
         if (document.documentElement.scrollTop > 70) {       // Se for maior que 0, fixa o Header e está na sessão Home
             setFixedChangeView(true);
-        } if (document.documentElement.scrollTop < 70) {
+        }
+        if (document.documentElement.scrollTop < 70) {
             setFixedChangeView(false);
-        } if (document.documentElement.scrollTop > 250) {
+        }
+        if (document.documentElement.scrollTop > 250) {
             setShowInfoHidden(true);
-        } if (document.documentElement.scrollTop < 250) {
+        }
+        if (document.documentElement.scrollTop < 250) {
             setShowInfoHidden(false);
         }
     }
@@ -88,19 +91,20 @@ function App() {
     }
 
     const handleKey = (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             getData();
         }
     }
     return (
         <div className="app">
             <div className={"header"}>
-                <GithubIcon />
+                <GithubIcon/>
 
                 <div className={"inputDiv"}>
-                    <input onKeyPress={handleKey} onChange={userSearch} className={"input"} type={"text"} placeholder={"Search for a user on Github"} />
+                    <input onKeyPress={handleKey} onChange={userSearch} className={"input"} type={"text"}
+                           placeholder={"Search for a user on Github"}/>
                     <div onClick={getData} className={"searchIcon"}>
-                        <SearchIcon />
+                        <SearchIcon/>
                     </div>
                 </div>
             </div>
@@ -109,21 +113,27 @@ function App() {
                 position: fixedChangeView && 'fixed',
                 width: fixedChangeView && '100%',
                 top: fixedChangeView && '0',
-            }} className={"changeView"}>
+            }} className={"changeView"}
+            >
                 {showInfoHidden &&
-                    <div className={"hiddenInfo"}>
-                        <img style={{borderRadius: '50%'}} width={"30px"} height={"30px"} src={user.avatar} />
-                        <span style={{color: '#C3C6CA', fontSize: 18, marginLeft: 10}}>{user.login}</span>
-                    </div>
+                <div className={"hiddenInfo"}>
+                    <img style={{borderRadius: '50%'}} width={"30px"} height={"30px"} src={user.avatar}/>
+                    <span style={{color: '#C3C6CA', fontSize: 18, marginLeft: 10}}>{user.login}</span>
+                </div>
                 }
-                <div onClick={() => setView('Overview')} style={{marginLeft: '30%', width: 150, borderBottomColor: view === 'Overview' && '#F9826C'}} className={"defaultBtn"}>
-                    <BookIcon fill={view === 'Overview' ? '#c3c6ca' : '#777'} />
+                <div onClick={() => setView('Overview')}
+                     style={{marginLeft: '30%', width: 150, borderBottomColor: view === 'Overview' && '#F9826C'}}
+                     className={"defaultBtn"}>
+                    <BookIcon fill={view === 'Overview' ? '#c3c6ca' : '#777'}/>
                     <span style={{color: view === 'Overview' && '#C3C6CA'}} className={"defaultText"}>Overview</span>
                 </div>
 
-                <div onClick={() => setView('Repositories')} style={{width: 220, borderBottomColor: view === 'Repositories' && '#F9826C'}} className={"defaultBtn"}>
-                    <BookmarkIcon fill={view === 'Repositories' ? '#c3c6ca' : '#777'} />
-                    <span style={{color: view === 'Repositories' && '#C3C6CA'}} className={"defaultText"}>Repositories</span>
+                <div onClick={() => setView('Repositories')}
+                     style={{width: 220, borderBottomColor: view === 'Repositories' && '#F9826C'}}
+                     className={"defaultBtn"}>
+                    <BookmarkIcon fill={view === 'Repositories' ? '#c3c6ca' : '#777'}/>
+                    <span style={{color: view === 'Repositories' && '#C3C6CA'}}
+                          className={"defaultText"}>Repositories</span>
                     <div className={"qtdRepos"}>
                         <span style={{color: '#999', fontSize: 14}}>{qtdRepos}</span>
                     </div>
@@ -202,54 +212,109 @@ function App() {
                 </div>
 
                 <div className={"side"}>
+                    {view == 'Overview' ?
 
-                    <div className={"arrayDiv"}>
-                        {repos.map((item, k) => (
+                        <div className={"arrayDiv"}>
+                            {repos.map((item, k) => (
 
-                            <div className={"arrayItem"} key={k}>
-                                <div className={"linkDiv"}>
-                                    <a className={"link"} href={item.html_url}>{item.name}</a>
-                                    <span className={"description"}>{item.description}</span>
-                                </div>
-
-                                <div className={"languageDiv"}>
-                                    {item.language &&
-                                    <>
-                                        <div style={{
-                                            backgroundColor:
-                                                item.language === 'JavaScript' && '#F1E05A' ||
-                                                item.language === 'Java' && '#B07219' ||
-                                                item.language === 'HTML' && '#E34C26' ||
-                                                item.language === 'Python' && '#3572A5' ||
-                                                item.language === 'GO' && '#00ADD8' ||
-                                                item.language === 'Shell' && '#89E051' ||
-                                                item.language === 'C' && '#555555' ||
-                                                item.language === 'PHP' && '#4F5D95' ||
-                                                item.language === 'PowerShell' && '#012456' ||
-                                                item.language === 'Pascal' && '#E3F171' ||
-                                                item.language === 'C++' && '#F34B7D' ||
-                                                item.language === 'Assembly' && '#6E4C13' ||
-                                                item.language === 'Ruby' && '#701516' ||
-                                                '#fff'
-                                        }} className={"ballFull"}/>
-
-                                        <span className={"language"}>{item.language}</span>
-                                    </>
-                                    }
-                                    <div className={"starDiv"}>
-                                        <StarIcon/>
-                                        <span className={"starText"}>{item.stargazers_count}</span>
+                                <div className={"arrayItem"} key={k}>
+                                    <div className={"linkDiv"}>
+                                        <a className={"link"} href={item.html_url}>{item.name}</a>
+                                        <span className={"description"}>{item.description}</span>
                                     </div>
 
-                                    <div className={"forkDiv"}>
-                                        <ForkIcon/>
-                                        <span className={"starText"}>{item.forks}</span>
+                                    <div className={"languageDiv"}>
+                                        {item.language &&
+                                        <>
+                                            <div style={{
+                                                backgroundColor:
+                                                    item.language === 'JavaScript' && '#F1E05A' ||
+                                                    item.language === 'Java' && '#B07219' ||
+                                                    item.language === 'HTML' && '#E34C26' ||
+                                                    item.language === 'Python' && '#3572A5' ||
+                                                    item.language === 'GO' && '#00ADD8' ||
+                                                    item.language === 'Shell' && '#89E051' ||
+                                                    item.language === 'C' && '#555555' ||
+                                                    item.language === 'PHP' && '#4F5D95' ||
+                                                    item.language === 'PowerShell' && '#012456' ||
+                                                    item.language === 'Pascal' && '#E3F171' ||
+                                                    item.language === 'C++' && '#F34B7D' ||
+                                                    item.language === 'Assembly' && '#6E4C13' ||
+                                                    item.language === 'Ruby' && '#701516' ||
+                                                    '#fff'
+                                            }} className={"ballFull"}/>
+
+                                            <span className={"language"}>{item.language}</span>
+                                        </>
+                                        }
+                                        <div className={"starDiv"}>
+                                            <StarIcon/>
+                                            <span className={"starText"}>{item.stargazers_count}</span>
+                                        </div>
+
+                                        <div className={"forkDiv"}>
+                                            <ForkIcon/>
+                                            <span className={"starText"}>{item.forks}</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                        :
+                        <div className={"arrayDivRepos"}>
+                            {repos.map((item, k) => (
+                                <div className={"repositories"}>
+                                    <div className={"reposSideDiv"}>
+                                        <a href={item.html_url} className={"link"}>{item.name}</a>
+                                        <span className={"description"}>{item.description}</span>
 
+                                        <div className={"languageDiv"}>
+                                            {item.language &&
+                                            <>
+                                                <div style={{
+                                                    backgroundColor:
+                                                        item.language === 'JavaScript' && '#F1E05A' ||
+                                                        item.language === 'Java' && '#B07219' ||
+                                                        item.language === 'HTML' && '#E34C26' ||
+                                                        item.language === 'Python' && '#3572A5' ||
+                                                        item.language === 'GO' && '#00ADD8' ||
+                                                        item.language === 'Shell' && '#89E051' ||
+                                                        item.language === 'C' && '#555555' ||
+                                                        item.language === 'PHP' && '#4F5D95' ||
+                                                        item.language === 'PowerShell' && '#012456' ||
+                                                        item.language === 'Pascal' && '#E3F171' ||
+                                                        item.language === 'C++' && '#F34B7D' ||
+                                                        item.language === 'Assembly' && '#6E4C13' ||
+                                                        item.language === 'Ruby' && '#701516' ||
+                                                        '#fff'
+                                                }} className={"ballFull"}/>
+
+                                                <span className={"language"}>{item.language}</span>
+                                            </>
+                                            }
+                                            <div className={"starDiv"}>
+                                                <StarIcon/>
+                                                <span className={"starText"}>{item.stargazers_count}</span>
+                                            </div>
+
+                                            <div className={"forkDiv"}>
+                                                <ForkIcon/>
+                                                <span className={"starText"}>{item.forks}</span>
+                                            </div>
+
+                                            <span style={{marginLeft: 15}} className={"starText"}>Updated on {item.updated_at}</span>
+                                        </div>
+                                    </div>
+
+                                    <div className={"btn"}>
+                                        <StarIcon />
+                                        <span>Star</span>
+                                    </div>
+
+                                </div>
+                            ))}
+                        </div>
+                    }
                 </div>
             </div>
         </div>
